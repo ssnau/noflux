0d66e54190a47df1059aaa70ef30936c2e39947f
liuxijin <liuxijin@meituan.com>
2015-10-26 11:57:42 +0800

    add connect

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var connect = require('./dist/connect');
+function connect(data) {
+  // 如果传入的是Component Class，则直接connect它
+  if (data && data.prototype && data.prototype.render) return connect(data);
+  return function(clazz) {
+    return connect(clazz, data);
+  }
+}
 module.exports = {
   // decorators
-  Connect: function (data) {
-    // 如果传入的是Component Class，则直接connect它
-    if (data && data.prototype && data.prototype.render) return connect(data);
-    return function(clazz) {
-      return connect(clazz, data);
-    }
-  },
+  Connect: connect,
+  connect: connect,
   state: require('./dist/state'),
   State: require('dataton')
 };
